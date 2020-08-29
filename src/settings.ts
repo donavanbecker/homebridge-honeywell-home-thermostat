73dc96237661c387a9ec600bc6c24545fb221bbf
Donavan Becker <donavan.becker@icloud.com>
2020-08-29 13:29:06 -0500

    v1.0.0
    ## [Version 1.0.0](https://github.com/donavanbecker/homebridge-honeywell-home-thermostat/releases/tag/v1.0.0 (2020-08-29)
    
    ### Major Changes
    
    - Changed Homebridge Honeywell Home Plugin into an all in one plugin, that supports Thermostats, Room Sensors, and Leak Sensors.
    
    ### Changes
    
    - Split off plugin to only support Thermostats and Thermostat Fans.

--- src/settings.ts
+++ src/settings.ts
@@ -1,12 +1,12 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HoneywellHome';
+export const PLATFORM_NAME = 'HoneywellHomeThermostat';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-honeywell-home';
+export const PLUGIN_NAME = 'homebridge-honeywell-home-thermostat';
 
 /**
  * This is the main url used to access honeywell API
